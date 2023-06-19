3aad204f2d3b5aa85e45ae4bcc432e2a380d1dea
Elghali1990 <elghali-mohamed@outlook.fr>
2023-06-19 08:56:30 +0100

    add project web

---
+++ park.web/Pages/Demande/Demandes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace park.web.Pages.Demande
+{
+    public class DemandesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
